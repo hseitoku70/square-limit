aa882ad89e881c4dfcb5e192cb7d11a7df08e0ae
Hanako Seitoku <hseitoku70@gmail.com>
2023-11-02 17:42:49 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main (main) where
 
-import Lib
+import Graphics.Gloss
+import SquareLimit
+
+window :: Display
+window = InWindow "Hello World" (640, 480) (100, 100)
 
 main :: IO ()
-main = someFunc
+main = display window white (wave frame0)
+
+frame0 :: Frame
+frame0 = Frame (0,0) (100,0) (0,100)
